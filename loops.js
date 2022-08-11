91eacbec9c0cb70966529cfed985decfb54e2753
koutscq <koutidivine@gmail.com>
2022-08-11 23:51:23 +0100

    first design completed

---
+++ loops.js
@@ -0,0 +1,29 @@
+// console.log("my first program");
+//  let x;
+//  for(let x=0;x<5;x++)
+//      console.log(x)
+// /let y
+//     while( y<=5)
+//      console.log(y)
+//  let x=4
+//  do{
+//          console.log(x)
+//      x++
+// /}
+//  while(x<10)
+// let x;
+// for (x = 0; x < 10; x++) {
+//   // console.log(x);
+//   if (x == 5) {
+//     continue;
+//     // break
+//   }
+//   console.log(x);
+// }
+// var student ={
+//     name: "fongan",
+//     age: "23",
+//     studies: "computer science"
+// }
+// let x = student.name + " of the age " + student.age + " studies" + student.studies;
+//   console.log(x)     
---
+++ objects/objectProperties.js
@@ -0,0 +1,8 @@
+// var student ={
+//     name: "fongan",
+//     age: "23",
+//     studies: "computer science"
+// }
+// student.age = 24
+// let x = student.name + " of the age " + student.age + " studies" + student.studies;
+//   console.log(x)   
